d35cbb7f205d00bb6c9a061a2e0d2093ad62b817
SteveWD1 <38817105+SteveWD1@users.noreply.github.com>
2018-04-30 01:44:38 +0100

    Add files via upload

---
+++ HELP.py
@@ -0,0 +1,18 @@
+# HELP MODULE TO IMPORT
+
+A = ("I'm looking for a flight but it's not listed here.")
+B = ("How do I book a flight?")
+
+
+def h_HELP():
+    print(A)
+    print("-"*40)
+    print("-"*40)
+    print("If your flight isn't listed here, it can be due to multiple reasons...")
+    print("        A.    Your connection to the internet might be limited, our servers work over the internet therefore flight information isn't being updated \n")
+    print("        B.    It could just be that there isn't an available flight for you at the moment, please wait for at least another day. \n")
+    print("        C.    Sometimes our systems doesn't display all flights")
+    print("              When tickets are sold out the system rarely displays the flights. Soon to be fixed \n")
+
+    
+
